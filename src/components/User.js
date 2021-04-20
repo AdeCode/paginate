0adafcfe2b04352ad94c0c6aeefa0ba279c60926
AdeCode <agorohabeeb@gmail.com>
2021-04-20 12:09:45 +0100

    initial commit

---
+++ src/components/User.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const User = ({user}) => {
+    return (
+        <div>
+            <hr/>
+            <p>{`Full Name: ${user.name.first} ${user.name.last}`}</p>
+            <p>{`Phone: ${user.phone}`}</p>
+
+        </div>
+    )
+}
+
+export default User
---
+++ src/components/users.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import User from './User';
+import {USER_PER_PAGE} from '../utils/constants'
+
+export default function user({users, page}) {
+    const startIndex = (page-1) * USER_PER_PAGE;
+    const selectedUsers = users.slice(startIndex, startIndex+USER_PER_PAGE);
+    return selectedUsers.map(user => (
+        <User user={user} key={user.login.uuid}/>
+    ))
+}
